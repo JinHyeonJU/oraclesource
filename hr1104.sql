973f1e67bd51a3beb65a5f0dbface08f227c3fc1
HyeonJu <guswn8557@gmail.com>
2020-11-04 16:45:32 +0900

    oracle updated

--- hr1104.sql
+++ hr1104.sql
@@ -1,1 +1,11 @@
 select * from tab;
+select * from employees;
+DESC EMPLOYEES;
+
+--사원번호가 176인 사람의 LAST_NAME, 부서번호 조회
+SELECT * FROM employees WHERE EMPLOYEES_ID = 176;
+
+-- 연봉이 12000 이상 (>=)되는 직원들의 LAST_NAME 및 연봉 조회
+
+
+-- 연봉이 5000에서 12000의 범위 이외의 사람들의 LAST_NAME 및 연봉 조회
