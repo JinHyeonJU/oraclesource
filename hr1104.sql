b5e06246d03fb7978684274823ef444623c1d06f
HyeonJu <guswn8557@gmail.com>
2020-11-04 16:51:51 +0900

    oracel updated

--- hr1104.sql
+++ hr1104.sql
@@ -3,9 +3,10 @@ select * from employees;
 DESC EMPLOYEES;
 
 --사원번호가 176인 사람의 LAST_NAME, 부서번호 조회
-SELECT * FROM employees WHERE EMPLOYEES_ID = 176;
+SELECT last_name, DEPARTMENT_ID FROM employees WHERE EMPLOYEE_ID =176;
 
 -- 연봉이 12000 이상 (>=)되는 직원들의 LAST_NAME 및 연봉 조회
-
+SELECT last_name, salary FROM employees WHERE salary>=12000;
 
 -- 연봉이 5000에서 12000의 범위 이외의 사람들의 LAST_NAME 및 연봉 조회
+SELECT LAST_NAME, salary FROM employees WHERE salary < 5000 or salary > 12000;
--- scott1104.sql
+++ scott1104.sql
@@ -1,1 +1,42 @@
+-- scott 계정이 가지고 있는 모돈 테이블 보기
 select * from tab;
+
+-- emp 테이블의 구조
+DESC emp;
+
+--emp 테이블 모든 내용 조회
+select * from emp;
+
+-- emp 테이블의 특정 필드만 조회
+SELECT ename, job, sal from emp;
+
+-- 중복 레코드 제거한 후 조회
+SELECT DISTINCT deptno FROM emp;
+
+-- alais(별칭) : AS
+SELECT empno AS 사원번호 FROM emp; 
+
+SELECT ename, sal, sal*12+comm AS 연봉, comm FROM emp;
+
+SELECT ename as 사원이름, job as "직 책" from emp;
+
+-- ORDER BY : 정렬하여 조회
+
+-- 하나의 열로 기준을 주어 오름차순 조회
+SELECT ename,sal FROM emp ORDER BY sal;
+
+-- 하나의 열로 기준을 주어 내림차순 조회
+SELECT ename,sal FROM emp ORDER BY sal DESC;
+
+-- 전체 데이터를 조회(사원번호 오름차순)
+SELECT * FROM emp ORDER BY empno;
+
+-- 부서번호의 오름차순, 부서번호가 동일하다면 급여의 내림차순
+SELECT * FROM emp ORDER BY deptno ASC, sal DESC;
+
+SELECT EMPNO AS EMPLOYEE_NO, ENAME AS EMPLOYEE_NAME,job, MGR AS MANAGER,
+hiredate, SAL AS SALARY, COMM AS COMMISIION, DEPTNO AS DEPARTMENT_NO from emp ORDER BY deptno DESC, ename ASC;
+
+SELECT * FROM emp WHERE empno = 7499 AND deptno = 30;
+SELECT * from emp WHERE deptno = 30 OR job = 'CLERK';
+SELECT * FROM emp WHERE deptno = 20 OR job = 'salesman';
